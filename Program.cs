ef2c7393948318f096756597c9e815de7900666b
deeptinirwal <66492566+deeptinirwal@users.noreply.github.com>
2020-08-15 10:20:17 -0400

    my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET22FinalExamEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
